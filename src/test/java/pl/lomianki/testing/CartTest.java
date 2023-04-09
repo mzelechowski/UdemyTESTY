14c487c352e799d7dfd756d5e5db326a68efad9c
Maciej Żelechowski <mzelechowski79@gmail.com>
2023-04-10 00:08:21 +0200

    chapter22-adnotacje@Disable@DisplayName

--- src/test/java/pl/lomianki/testing/CartTest.java
+++ src/test/java/pl/lomianki/testing/CartTest.java
@@ -1,13 +1,16 @@
 package pl.lomianki.testing;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+// @Disabled - can be set either before class or selected test
+@DisplayName("Cart is able to process 1000 orders in 100ms.")
 class CartTest {
-
+   // @Disabled -can be set either before class or selected test
     @Test
     void simulateLargeOrder() {
         //given
