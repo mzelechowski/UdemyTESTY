69ae59371812b5afbf8b1058f971117bb7093e08
Maciej Żelechowski <mzelechowski79@gmail.com>
2023-04-10 00:42:48 +0200

    chapter23-matchery do łączenia asercji

--- src/main/java/pl/lomianki/testing/Cart.java
+++ src/main/java/pl/lomianki/testing/Cart.java
@@ -5,21 +5,27 @@ import java.util.List;
 
 public class Cart {
     private List<Order> orders = new ArrayList<>();
-    void addOrderToCart(Order order){
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    void addOrderToCart(Order order) {
         this.orders.add(order);
     }
-    void clearCart(){
+
+    void clearCart() {
         this.orders.clear();
     }
 
-    void simulateLargeOrder(){
-        for (int i = 0; i < 1000 ; i++) {
-            Meal meal= new Meal(i%10, "Hamburger no "+i);
+    void simulateLargeOrder() {
+        for (int i = 0; i < 1000; i++) {
+            Meal meal = new Meal(i % 10, "Hamburger no " + i);
             Order order = new Order();
             order.addMealToOrder(meal);
             addOrderToCart(order);
         }
-        System.out.println("Cart size: "+orders.size());
+        System.out.println("Cart size: " + orders.size());
         clearCart();
     }
 
--- src/test/java/pl/lomianki/testing/CartTest.java
+++ src/test/java/pl/lomianki/testing/CartTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 // @Disabled - can be set either before class or selected test
 @DisplayName("Cart is able to process 1000 orders in 100ms.")
 class CartTest {
-   // @Disabled -can be set either before class or selected test
+    // @Disabled -can be set either before class or selected test
     @Test
     void simulateLargeOrder() {
         //given
@@ -20,5 +23,38 @@ class CartTest {
         //then
 //        assertTimeout(Duration.ofMillis(10),()->cart.simulateLargeOrder());
         assertTimeout(Duration.ofMillis(100), cart::simulateLargeOrder);
+    }
+
+    @Test
+    void cartShouldNotBeEmptyAfterAddingOrderToCart() {
+        //given
+        Order order = new Order();
+        Cart cart = new Cart();
+
+        //when
+        cart.addOrderToCart(order);
+
+        //then
+        assertThat(cart.getOrders(), anyOf(
+                notNullValue(),
+                hasSize(1),
+                is(not(empty())),
+                is(not(emptyCollectionOf(Order.class)))
+        ));
+
+        assertThat(cart.getOrders(), allOf(
+                notNullValue(),
+                hasSize(1),
+                is(not(empty())),
+                is(not(emptyCollectionOf(Order.class)))
+        ));
+
+        assertAll("This is a group assertions for cart",
+                () -> assertThat(cart.getOrders(), notNullValue()),
+                () -> assertThat(cart.getOrders(), hasSize(1)),
+                () -> assertThat(cart.getOrders(), is(not(empty()))),
+                () -> assertThat(cart.getOrders(), is(not(emptyCollectionOf(Order.class)))),
+                () -> assertThat(cart.getOrders().get(0).getMeals(), empty())
+        );
     }
 }
